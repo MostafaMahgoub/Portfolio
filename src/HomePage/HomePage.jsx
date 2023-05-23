8bbe4f0ffa312a537d8cfd1429d9fe9dc5480be9
MostafaR <mostafamahgoub@outlook.com>
2023-05-23 22:20:33 +0300

    Added the switch button in homepage

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -8,6 +8,16 @@ function HomePage() {
       <video autoPlay muted loop id="bg-video">
         <source src={bgVideo} type="video/mp4" />
       </video>
+        <div className="round">
+          <input type="checkbox" id="onoff" name="onoff" />
+          <div className="back">
+            <label className="but" htmlFor="onoff">
+              <span className="on">I</span>
+              <span className="off">0</span>
+            </label>
+          </div>
+        </div>
+
     </div>
   );
 }
