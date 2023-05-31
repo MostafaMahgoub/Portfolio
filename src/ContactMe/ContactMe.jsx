131baa8087940e13d4766a380203bd94938c0d2d
MostafaR <mostafamahgoub@outlook.com>
2023-05-31 13:43:17 +0300

    Added the heading animation for ContactMe section

--- src/ContactMe/ContactMe.jsx
+++ src/ContactMe/ContactMe.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React , {useRef , useEffect} from 'react';
 import bgVideo from './assets/videos/ContactMe.mp4';
 import './ContactMe.sass';
+import ContactmeHeading from './components/ContactmeHeading';
 
 function ContactMe({ContactMePage}) {
+  const ContactMeSectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          ContactMeSectionRef.current.classList.add('ContactMe-scroll-effect');
+        } else {
+          ContactMeSectionRef.current.classList.remove('ContactMe-scroll-effect');
+        }
+      },
+      {
+        rootMargin: '100px',
+      }
+    );
+    const ProjectsRef = ContactMeSectionRef;
+    observer.observe(ProjectsRef.current);
+
+    return () => {
+      observer.unobserve(ProjectsRef.current);
+    };
+  }, []);
+
   return (
     <div ref={ContactMePage} className="ContactMe">
       <video autoPlay muted loop id="bg-video">
         <source src={bgVideo} type="video/mp4" />
       </video>
+      <ContactmeHeading ContactMeSectionRef={ContactMeSectionRef} />
     </div>
   );
 }
---
+++ src/ContactMe/components/ContactmeHeading.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ContactmeHeading({ ContactMeSectionRef }) {
+  return <h1 className="ContactMe-heading" ref={ContactMeSectionRef}>Contact Me</h1>;
+}
+
+export default ContactmeHeading;
