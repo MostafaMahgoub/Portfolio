c944c7a3f46b09a6ba78ace1250cfc860342477f
MostafaR <mostafamahgoub@outlook.com>
2023-05-31 11:56:03 +0300

    Finished the learning curve in expertise section

--- src/SkillsSection/SkillsSection.jsx
+++ src/SkillsSection/SkillsSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import bgVideo from './assets/videos/SkillsSection.mp4';
 import './SkillsSection.sass';
+import Chart from './components/Chart';
 
 function SkillsSection({SkillsPageRef}) {
   return (
@@ -8,6 +9,7 @@ function SkillsSection({SkillsPageRef}) {
       <video autoPlay muted loop id="bg-video">
         <source src={bgVideo} type="video/mp4" />
       </video>
+      <Chart />
     </div>
   );
 }
---
+++ src/SkillsSection/components/Chart.jsx
@@ -0,0 +1,109 @@
+import React, { useRef, useEffect } from 'react';
+import { lightningChart, PointShape, Themes } from '@arction/lcjs';
+
+function Chart() {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      
+      const chart = lightningChart().ChartXY({
+        container: chartRef.current,
+        theme: Themes.cyberSpace,
+      });
+
+      
+      chart
+        .setTitle('Learning curve')
+        .setPadding({ top: 50, right: 40, bottom: 40, left: 60 });
+
+     
+      chart.getDefaultAxisX().setInterval({ start: 2018, end: 2023 }).setTitle('Year');
+      chart.getDefaultAxisY().setInterval({ start: 0, end: 200 }).setTitle('Rate of improvment');
+
+      
+      const pointSize = 10;
+      const skillSeries = [
+        { name: 'Javascript,HTML,CSS', data: [
+          { x: 2020, y: 0 },
+          { x: 2021, y: 50 },
+          { x: 2022, y: 50 },
+          { x: 2023, y: 150 },
+        ]},
+        { name: 'React,Sass', data: [
+          { x: 2020, y: 0 },
+          { x: 2021, y: 0 },
+          { x: 2022, y: 50 },
+          { x: 2023, y: 150 },
+        ]},
+        { name: 'Dart,Flutter,Firebase', data: [
+          { x: 2020, y: 0 },
+          { x: 2021, y: 20 },
+          { x: 2022, y: 100 },
+        ]},
+        { name: 'Unity,PHP', data: [
+          { x: 2020, y: 0 },
+          { x: 2021, y: 50 },
+        ]},
+        { name: 'C++', data: [
+          { x: 2018, y: 0 },
+          { x: 2019, y: 50 },
+          { x: 2020, y: 100 },
+          { x: 2021, y: 150 },
+        ]},
+        { name: 'MySQL,MongoDB', data: [
+          { x: 2019, y: 0 },
+          { x: 2020, y: 50 },
+          { x: 2021, y: 80 },
+          { x: 2022, y: 150 },
+          { x: 2023, y: 200 },
+        ]},
+        { name: 'Python', data: [
+          { x: 2020, y: 0 },
+          { x: 2021, y: 30 },
+          { x: 2022, y: 50 },
+          { x: 2023, y: 100 },
+        ]},
+        { name: 'ElectronJS,NodeJS,Bootstrap', data: [
+          { x: 2021, y: 0 },
+          { x: 2022, y: 10 },
+          { x: 2023, y: 200 },
+        ]},
+        { name: 'Git,GitHub', data: [
+          { x: 2020, y: 0 },
+          { x: 2021, y: 50 },
+          { x: 2022, y: 80 },
+          { x: 2023, y: 150 },
+        ]},
+      ];
+      
+      skillSeries.forEach((series, index) => {
+        chart
+          .addPointLineSeries({
+            pointShape: PointShape.Triangle
+          })
+          .setName(series.name)
+          .setPointSize(pointSize)
+          .add(series.data);
+      });
+
+      
+      chart.setAutoCursor((cursor) =>
+        cursor.setResultTableAutoTextStyle(true).setTickMarkerXAutoTextStyle(true).setTickMarkerYAutoTextStyle(true),
+      );
+
+      
+      const legendSkills = chart.addLegendBox().add(chart);
+      legendSkills.setTitle('Skills');
+
+      
+      return () => {
+        chart.dispose();
+      };
+    }
+  }, [chartRef]);
+
+  return <div ref={chartRef} className='chart_Container' />;
+}
+
+export default Chart;
