be1138a4981038cd214bb4100311bddab8dff9ac
MostafaR <mostafamahgoub@outlook.com>
2023-06-01 15:58:50 +0300

    put icons in separate file

---
+++ src/SkillsSection/components/icons.js
@@ -0,0 +1,45 @@
+import githubIcon from '../assets/icons/github.svg';
+import reactIcon from '../assets/icons/react.svg';
+import sassIcon from '../assets/icons/sass.svg';
+import jsIcon from '../assets/icons/javascript.svg';
+import cssIcon from '../assets/icons/css.svg';
+import htmlIcon from '../assets/icons/html.svg';
+import mysqlIcon from '../assets/icons/mysql.svg';
+import mongoIcon from '../assets/icons/mongo.svg';
+import dartIcon from '../assets/icons/dart.svg';
+import flutterIcon from '../assets/icons/flutter.svg';
+import firebaseIcon from '../assets/icons/firebase.svg';
+import cppIcon from '../assets/icons/c++.svg';
+import pythonIcon from '../assets/icons/python.svg';
+import ElectronIcon from '../assets/icons/electronjs.svg';
+import NodeIcon from '../assets/icons/nodejs.svg';
+import bootstrapIcon from '../assets/icons/bootstrap.svg';
+import gitIcon from '../assets/icons/git.svg';
+import unityIcon from '../assets/icons/unity.svg';
+import phpIcon from '../assets/icons/php.svg';
+
+
+const skills = [
+    { skillName: 'ReactJS', icon: reactIcon },
+    { skillName: 'CSS', icon: cssIcon },
+    { skillName: 'Sass', icon: sassIcon },
+    { skillName: 'Javascript', icon: jsIcon},
+    { skillName: 'HTML', icon: htmlIcon },
+    { skillName: 'MySQL', icon: mysqlIcon },
+    { skillName: 'MongoDB', icon: mongoIcon},
+    { skillName: 'Dart', icon: dartIcon},
+    { skillName: 'Flutter', icon: flutterIcon},
+    { skillName: 'Firebase', icon: firebaseIcon },
+    { skillName: 'C++', icon: cppIcon},
+    { skillName: 'Python', icon: pythonIcon },
+    { skillName: 'ElectronJS', icon: ElectronIcon },
+    { skillName: 'NodeJS', icon: NodeIcon },
+    { skillName: 'Bootstrap', icon: bootstrapIcon },
+    { skillName: 'Git', icon: gitIcon },
+    { skillName: 'Github', icon: githubIcon },
+    { skillName: 'Unity', icon: unityIcon },
+    { skillName: 'PHP', icon: phpIcon },
+  ];
+
+
+export default skills;
