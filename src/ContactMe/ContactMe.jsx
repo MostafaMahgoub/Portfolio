1829e2d714fc13a1bd381a4fb3e5d7d205421cdc
MostafaR <mostafamahgoub@outlook.com>
2023-06-09 11:49:46 +0300

    Added the slide container

--- src/ContactMe/ContactMe.jsx
+++ src/ContactMe/ContactMe.jsx
@@ -1,22 +1,32 @@
-import React , {useRef , useEffect} from 'react';
+import React , {useRef , useEffect , useState} from 'react';
 import './ContactMe.sass';
 import ContactmeHeading from './components/ContactmeHeading';
 import BackgroundVideo from './components/BackgroundVideo';
 
 function ContactMe({ContactMePage}) {
   const ContactMeSectionRef = useRef(null);
+  const [showContactMe, setShowContactMe] = useState(false);
+  const [isInView, setIsInView] = useState(false);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
-        if (entry.isIntersecting) {
+        if (entry.isIntersecting && !isInView) {
+
           ContactMeSectionRef.current.classList.add('ContactMe-scroll-effect');
-        } else {
+          setShowContactMe(true);
+          setIsInView(true);
+
+        } else if (!entry.isIntersecting && isInView) {
+
           ContactMeSectionRef.current.classList.remove('ContactMe-scroll-effect');
+          setShowContactMe(false);
+          setIsInView(false);
+
         }
       },
       {
-        rootMargin: '100px',
+        rootMargin: '800px 0px 150px 0px',
       }
     );
     const ProjectsRef = ContactMeSectionRef;
@@ -25,12 +35,17 @@ function ContactMe({ContactMePage}) {
     return () => {
       observer.unobserve(ProjectsRef.current);
     };
-  }, []);
+  }, [isInView]);
 
   return (
     <div ref={ContactMePage} className="ContactMe">
       <BackgroundVideo />
       <ContactmeHeading ContactMeSectionRef={ContactMeSectionRef} />
+      <div
+        className={`ContactMe-container ${
+          showContactMe ? 'show-ContactMe-container' : ''
+        }`}
+      ></div>
     </div>
   );
 }
