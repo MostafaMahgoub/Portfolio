4c2c6e4bcb0b279a20fa7c39a606f3e0423822b7
MostafaR <mostafamahgoub@outlook.com>
2023-06-14 19:16:47 +0300

    Update lat,long positions

--- src/ContactMe/components/Map.jsx
+++ src/ContactMe/components/Map.jsx
@@ -15,12 +15,11 @@ const googleHybrid = L.tileLayer('https://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}
 });
 
 function Map() {
-  const position = [30.057698584217924, 31.35537700118351];
-  const center = [30.05796160624196, 31.355095496468316];
+  const position = [30.057877343620987, 31.352824810113287];
 
   return (
     <div className="Map-container">
-      <MapContainer center={center} zoom={15} style={{ height: '100%', width: '100%', borderRadius: '5px' }} attributionControl={false} zoomControl={false}>
+      <MapContainer center={position} zoom={15} style={{ height: '100%', width: '100%', borderRadius: '5px' }} attributionControl={false} zoomControl={false}>
         <TileLayer
           url={googleHybrid._url}
           maxZoom={googleHybrid.options.maxZoom}
